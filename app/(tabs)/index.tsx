3a5ad7822ef161bda03a95eb5e302bcd6d05c655
RonakKedia7 <ronakkedia0@gmail.com>
2026-03-29 09:13:49 +0530

    Implement font(PlusJakartaSans)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,27 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-5xl font-bold font-sans-extrabold">Home</Text>
       <Link
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
         href="/(auth)/sign-in"
       >
         Go to Sign In
       </Link>
       <Link
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
         href="/(auth)/sign-up"
       >
         Go to Sign Up
       </Link>
-
-      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
-    </View>
+    </SafeAreaView>
   );
 }
